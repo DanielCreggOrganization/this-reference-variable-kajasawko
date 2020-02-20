558a700fb37de5c4cf69ceb4c28922835e97db3e
kajasawko <kaja.sawko@gmail.com>
2020-02-20 14:49:29 +0000

    constructors

--- Date.java
+++ Date.java
@@ -4,30 +4,54 @@
 public class Date {
     
     // Instance Variables
-    private int day;
-    private int month;
-    private int year;
+     int day;
+     int month;
+     int year;
+     String event;
 
     // Default Constructor (No Parameters)
     public Date(){
-        //Your code here
+        day = 1;
+        month = 1;
+        year = 1;
+        event = "Nothing on today";
     }
 
-    // Parameterized constructor (3 Parameters)
-    public Date(int newDay, int newMonth, int newYear){
-        // Your code here.
+    // Parameterized constructor (1 Parameter)
+    public Date(int day){
+        this.day = day;
+    }
+
+     // Parameterized constructor (2 Parameters)
+    public Date(int day, int month){
+        this(day);
+        this.month = month;
     }
     
-    // Parameterized constructor (2 Parameters)
-    // Your code here.
+
+    // Parameterized constructor (3 Parameters)
+    public Date(int day, int month, int year){
+        this(day, month);
+        this.year = year;
+    }
+
+
+    // Parameterized constructor (3 Parameters)
+    public Date(int day, int month, int year, String event){
+        this(day, month, year);
+        this.event = event;
+    }
     
-    // Parameterized constructor (1 Parameter)
-    // Your code here.
+
     
+
+    
+
     // A simple print method
     public void printDate(){
         // Print the date like this: day/month/year
         // Your code here
+        System.out.println(day +"/"+ month+"/"+year+" "+event);
     }
 
 }
--- Main.java
+++ Main.java
@@ -4,7 +4,27 @@
 public class Main {
     public static void main(String[] args) {
         // Create a Date object using the default constructor
+        Date dateObject1 = new Date();
+        //System.out.println(dateObject1.year);
+
         // Create 3 more Date objects using all the paramiterised constructors
+        Date dateObject2 = new Date(20, 2, 2020);
+
+        Date dateObject3 = new Date(2000);
+        System.out.println(dateObject3.year);
+        System.out.println(dateObject3.day);
+
+        Date dateObject4 = new Date(15, 12);
+        System.out.println(dateObject4.month);
+
+        Date dateObject5 = new Date(17, 3, 2020, "Paddys Day");
+
+        System.out.println(dateObject5.event);
+        System.out.println(dateObject5.day);
+
         // Call the printDate() method for each object you create.
+        dateObject5.printDate();
     }
 }
+
+
